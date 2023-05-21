8f9407b2bad40d38592015bd68002f344df7280f
Ankush <alamba570@gmail.com>
2023-05-21 12:49:09 +0530

    1

--- Programming skills/Plus One.java
+++ Programming skills/Plus One.java
@@ -1,15 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length; i>=0; i--){
-            if(digits[i]<9){
-                digits[i]++;
+        int n = digits.length;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++; 
                 return digits;
             }
-            digits[i]=0;
+           
+            else {
+                digits[i] = 0; 
+            }
         }
 
-        digits = new int[digits.length-1];
-        digits[0]=1;
-        return digits;
+        int[] result = new int[n + 1]; 
+        result[0] = 1;
+        return result; 
     }
 }
