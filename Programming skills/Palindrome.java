6caa6f973330ffaa992ac43a7bb8fed92074c479
Ankush <alamba570@gmail.com>
2023-06-12 13:44:52 +0530

    palindrome

---
+++ Programming skills/Palindrome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class palindrome{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev = "";
+        int len = str.length();
+        for(int i = len-1; i>=0; i--){
+            rev = rev + str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
