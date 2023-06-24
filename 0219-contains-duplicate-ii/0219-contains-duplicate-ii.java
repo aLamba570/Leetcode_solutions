1934b1e7bbf7d287d81e80e057f13f8ce3996578
Ankush Lamba <alamba570@gmail.com>
2023-06-24 19:42:08 +0530

    Time: 1599 ms (8.43%), Space: 54.8 MB (61.27%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<Math.min(i + k + 1, n); j++){
+                if(nums[i] == nums[j] ){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
