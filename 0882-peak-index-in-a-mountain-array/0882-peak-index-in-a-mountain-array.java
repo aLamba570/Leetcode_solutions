ac536bb2a7ce34a2bec54bc33fd3d741f677e305
Ankush Lamba <alamba570@gmail.com>
2023-06-27 20:08:56 +0530

    Time: 1 ms (15.88%), Space: 55.4 MB (69.5%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] nums) {
+        int maxIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
