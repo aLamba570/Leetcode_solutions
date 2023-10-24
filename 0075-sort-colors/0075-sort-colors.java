0977bce40dfd3b93c78c9e6bf5a87c57f5ecaad8
Ankush Lamba <alamba570@gmail.com>
2023-10-24 12:22:17 +0530

    Time: 2 ms (13.96%), Space: 40.3 MB (98.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,34 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums, 0 , nums.length-1);
-    }
+        boolean swapped;
 
-    public void quickSort(int[] nums, int low, int high){
-        if(low<high){
-            int pidx = partition(nums, low, high);
-
-            quickSort(nums, low, pidx-1);
-            quickSort(nums, pidx+1, high);
-        }
-    }
-
-    public static int partition(int [] nums, int low, int high){
-        int pivot = nums[high];
-        int i = low -1;
-
-        for(int j = low; j<high; j++){
-            if(nums[j] < pivot){
-                i++;
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        for(int i = 0; i<nums.length-1; i++){
+            swapped = false;
+            for(int j = 0; j<nums.length-i-1; j++){
+                if(nums[j]> nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
+                }
             }
+            
+            
+            if(!swapped){
+                break;
+           }
         }
-        i++;
-
-        int temp = nums[i];
-        nums[i] = nums[high];
-        nums[high] = temp;
-        return i;
     }
 }
