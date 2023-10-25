de611d4d68cfd6a46fda129c6900e6f5521a6b4a
Ankush Lamba <alamba570@gmail.com>
2023-10-25 17:14:51 +0530

    Time: 2 ms (65.24%), Space: 44.8 MB (28.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,84 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] merged = new int[m + n];
-        int i = 0, j = 0, k = 0;
+    
+    int m = nums1.length;
+    int n = nums2.length;
 
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                merged[k++] = nums1[i++];
-            } else {
-                merged[k++] = nums2[j++];
-            }
-        }
-
-        while (i < m) {
-            merged[k++] = nums1[i++];
-        }
-
-        while (j < n) {
-            merged[k++] = nums2[j++];
-        }
-
-        int length = merged.length;
-
-        if (length % 2 == 0) {
-            int mid1 = merged[length / 2 - 1];
-            int mid2 = merged[length / 2];
-            return (double) (mid1 + mid2) / 2;
+    if (nums1.length == 0) {
+        if (nums2.length % 2 == 0) {
+            return (nums2[nums2.length / 2 - 1] + nums2[nums2.length / 2]) / 2.0;
         } else {
-            return (double) merged[length / 2];
+            return nums2[nums2.length / 2];
+        }
+    } else if (nums2.length == 0) {
+        if (nums1.length % 2 == 0) {
+            return (nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]) / 2.0;
+        } else {
+            return nums1[nums1.length / 2];
         }
     }
 
+    int z = m+n;
+
+    int [] merged = new int[z];
+    int i = 0, j = 0, k=0;
+
+    while(i<m && j<n){
+        if(nums1[i]<nums2[j]){
+            merged[k]=nums1[i];
+            k++;
+            i++;
+        }
+        else{
+            merged[k]=nums2[j];
+            k++;
+            j++;
+        }
+    }
+
+    while(i<m){
+        merged[k]=nums1[i];
+        k++;
+        i++;
+    }
+
+    while(j<n){
+        merged[k]= nums2[j];
+        k++;
+        j++;
+    }
+
+    if(z%2==0){
+        int mid1 = merged[z/2-1];
+        int mid2 = merged[z/2];
+        return (double) (mid1+mid2)/2;
+    }
+    else{
+        return (double) merged[z/2];
+    }
+
+    // ArrayList<Integer> list1 = new ArrayList<>();
+    // ArrayList<Integer> list2 = new ArrayList<>();
+
+    // for(int num: nums1){
+    //     list1.add(num);
+    // }
+    // for(int num: nums2){
+    //     list2.add(num);
+    // }
+
+    // list1.addAll(list2);
+
+    // Collections.sort(list1);
+
+    // int p = list1.size();
+
+    //     if (p % 2 == 0) {
+    //         int mid1 = list1.get(p / 2 - 1);
+    //         int mid2 = list1.get(p / 2);
+    //         return (double) (mid1 + mid2) / 2;
+    //     } else {
+    //         return (double) list1.get(p / 2);
+    //     }
+    }
 }
