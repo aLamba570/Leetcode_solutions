4257961c0ad80dc56bcccfa6e4fb6f7a4190d044
Ankush Lamba <alamba570@gmail.com>
2023-10-25 11:35:17 +0530

    Time: 21 ms (31.43%), Space: 43.5 MB (48.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Remove non-alphanumeric characters and convert to lowercase
-        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String str = s.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        String rev = "";
+        String reversed = new StringBuilder(str).reverse().toString();
 
-        // Build the reverse string
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev = rev + str.charAt(i);
-        }
-
-        // Compare the original string and its reverse
-        return str.equals(rev);
+        return str.equals(reversed);
+        
     }
 }
