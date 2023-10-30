c3ad964408c4e9c6ae5a0d508fb2e38768719590
Ankush Lamba <alamba570@gmail.com>
2023-10-30 10:32:18 +0530

    Time: 4 ms (100%), Space: 42.9 MB (31.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,37 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        String str = Integer.toString(x);
 
-        String rev = new StringBuilder(str).reverse().toString();
+        //less optimized way
 
-        return str.equals(rev);
+
+        // String str = Integer.toString(x);
+
+        // String rev = "";
+
+        // for (int i = str.length() - 1; i >= 0; i--) {
+        //     rev += str.charAt(i);
+        // }
+
+
+        // return str.equals(rev);
+
+
+        //more optimized
+
+        if(x<0){
+            return false;
+        }
+
+        int orignal = x;
+        int rev = 0;
+
+        while(x!=0){
+            int digit = x%10;
+            rev = rev*10 + digit;
+            x = x/10;
+        }
         
+        return orignal == rev;
     }
 }
