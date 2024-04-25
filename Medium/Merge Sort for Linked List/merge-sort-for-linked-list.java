a21de9c46d28e98dd2c9104e50eba3db54859f0a
Ankush Lamba <alamba570@gmail.com>
2024-04-25 21:36:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
+++ Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -74,18 +74,56 @@ class Node
 class Solution {
     // Function to sort the given linked list using Merge Sort.
     static Node mergeSort(Node head) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Node temp = head ;
-        while(temp != null){
-            list.add(temp.data);
-            temp = temp.next ;
+        if(head == null || head.next==null){
+            return head;
         }
-        Collections.sort(list);
-        temp = head ;
-        for(int i = 0 ; i < list.size() ; i++){
-            temp.data = list.get(i);
-            temp = temp.next ;
+
+        Node middle = findMiddle(head);
+        Node nextMiddle = middle.next;
+        middle.next = null;
+
+        Node left = mergeSort(head);
+        Node right = mergeSort(nextMiddle);
+
+        return mergeList(left, right);
+    }
+
+    private static Node findMiddle(Node head){
+        Node fast = head;
+        Node slow = head;
+        Node prev = null;
+
+        while(fast!= null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        return prev;
+    }
+
+    private static Node mergeList(Node left, Node right){
+        Node dummy = new Node(-1);
+        Node current = dummy;
+
+        while(left != null && right != null){
+            if(left.data<right.data){
+                current.next = left;
+                left = left.next;
+            }
+            else{
+                current.next = right;
+                right = right.next;
+            }
+            current= current.next;
+        }
+
+        if(left != null){
+            current.next = left;
+        }
+
+        if(right!= null){
+            current.next = right;
+        }
+        return dummy.next;
     }
 }
