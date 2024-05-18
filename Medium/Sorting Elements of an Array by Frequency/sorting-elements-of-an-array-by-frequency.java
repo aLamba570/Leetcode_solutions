ca2daf8fa3c46f6aae2f14ece359f96962470bfd
Ankush Lamba <alamba570@gmail.com>
2024-05-18 18:28:08 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
+++ Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
@@ -1,54 +1,75 @@
-/*package whatever //do not write package name here */
+//{ Driver Code Starts
+//Initial Template for Java
 
 import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.Map.Entry;
 
-class GFG {
-	public static void main (String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		
-		while(t-->0){
-		    int n = sc.nextInt();
-		    int [] nums = new int[n];
-		    for (int i = 0; i < n; i++) {
-                nums[i] = sc.nextInt();
-            }
-		    
-		    
-		   HashMap<Integer, Integer> map = new HashMap<>();
-		    
-		    for(int num : nums){
-		        map.put(num, map.getOrDefault(num, 0) + 1);
-		    }
-		    
-		    List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-		    
-            list.sort((entry1, entry2) -> {
-                int freqComparison = entry2.getValue().compareTo(entry1.getValue());
-                if (freqComparison == 0) {
-                    return entry1.getKey().compareTo(entry2.getKey());
-                }
-                return freqComparison;
-            });
-            
-		    int [] result = new int[n];
-		    int index = 0;
-		    for(Map.Entry<Integer, Integer> mod : list){
-		        int number = mod.getKey();
-		        int freq = mod.getValue();
-		        for (int i = 0; i < freq; i++) {
-                    result[index++] = number;
-                }
-		    }
-		    
-		    for (int num : result) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-		}
+
+class Driverclass 
+{
+	public static void main(String[] args) throws NumberFormatException, IOException
+	{
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(sc.readLine());
+	    while(n != 0)
+		{
+			int size = Integer.parseInt(sc.readLine());
+			int arr[] = new int[size];
+			String[] temp = sc.readLine().trim().split("\\s+");
 			
+			for(int i = 0; i < size; i++)
+			    arr[i] = Integer.parseInt(temp[i]);
+			    
+			 ArrayList<Integer> ans = new ArrayList<Integer>();
+			 ans = new Solution().sortByFreq(arr, size);
+			 for(int i=0;i<ans.size();i++)
+			    System.out.print(ans.get(i)+" ");
+		    System.out.println();
+			n--;
+		}
 	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to sort the array according to frequency of elements.
+    static ArrayList<Integer> sortByFreq(int arr[], int n)
+    {
+        // add your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int num: arr){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        
+        list.sort((entry1, entry2) -> {
+            int freq = entry2.getValue().compareTo(entry1.getValue());
+            if(freq == 0){
+                return entry1.getKey().compareTo(entry2.getKey());
+            }
+            return freq;
+        });
+        
+        
+        ArrayList<Integer> listt = new ArrayList<>();
+        int index = 0;
+        for(Map.Entry<Integer, Integer> mod : list){
+            int num = mod.getKey();
+            int freqC = mod.getValue();
+            for(int i = 0; i<freqC; i++){
+                listt.add(num);
+            }
+        }
+        return listt;
+    }
 }
