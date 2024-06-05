bd51897786c59b980aadbae42d0946a46544f59f
Ankush Lamba <alamba570@gmail.com>
2024-06-05 22:51:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Length of the longest substring/length-of-the-longest-substring.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String s = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestUniqueSubsttr(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    int longestUniqueSubsttr(String s){
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        while(right<s.length()){
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                count = Math.max(count, right-left+1);
+                right++;
+            }
+            else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return count;
+    }
+}
