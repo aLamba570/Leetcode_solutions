dea0e9b47011dfbb08217db93822420dee225f65
Ankush Lamba <alamba570@gmail.com>
2024-07-24 16:46:02 +0530

    Time: 0 ms (100%), Space: 40.4 MB (81.68%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n <= 0) {
-            return false;
+        while (n>1){
+            if (n%5==0)             n=n/5;
+            else if (n%3==0)        n=n/3;
+            else if (n%2==0)        n=n/2;
+            else                    break;
         }
-
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        while (n % 5 == 0) {
-            n /= 5;
-        }
-
-        return n == 1;
+        return n==1;
     }
 }
