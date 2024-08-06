381a4e34e87cca9f1235e25170a8fbc60ea46896
Ankush Lamba <alamba570@gmail.com>
2024-08-06 18:52:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -6,6 +6,10 @@ class Solution {
             rev =rev *10+ x%10;
             x/=10;
         }
+
+        if(rev>Integer.MAX_VALUE && rev< Integer.MIN_VALUE){
+            return 0;
+        }
         return (int) rev;
     }
 }
