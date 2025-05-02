d880a960891d0ed569a4d721da56d07c31fe0083
Ankush Lamba <alamba570@gmail.com>
2025-05-02 10:57:47 +0530

    Time: 1 ms (73.77%), Space: 45.4 MB (93.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,31 +3,42 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> columns = new HashSet<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    columns.add(j);
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    setRow(matrix, i);
+                    setCol(matrix, j);
                 }
             }
         }
 
-        //set rows to zero
-
-        for(int row: rows){
+        for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                matrix[row][j]=0;
+                if(matrix[i][j] == 999){
+                    matrix[i][j] = 0;
+                }
             }
         }
+    }
 
-        //set columns to zero
+    public static void setRow(int[][] matrix, int i){
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for (int col : columns) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][col] = 0;
+        for(int j = 0; j<n; j++){
+            if(matrix[i][j] !=0){
+                matrix[i][j] = 999;
+            }
+        }
+    }
+
+    public static void setCol(int[][] matrix, int j){
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for(int i = 0; i<m; i++){
+            if(matrix[i][j] !=0){
+                matrix[i][j] = 999;
             }
         }
     }
